ad945fb5086eec65710b9880af4935126d0ca427
Priyanshu Bhandari <technopriyu@gmail.com>
2024-09-15 17:40:42 +0530

    init rn with clerk

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+// import { ThemedText as Text } from "@/components/ThemedText";
+// import { ThemedView as View } from "@/components/ThemedView";
+
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const IndexScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>ScreenOne</Text>
+    </SafeAreaView>
+  );
+};
+
+export default IndexScreen;
